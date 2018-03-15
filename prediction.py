c95a373320847dc8ff668d2c0e4988f0b7310ff0
Eric <zixia@umail.ucsb.edu>
2018-03-14 18:02:57 -0700

    first

--- prediction.py
+++ prediction.py
@@ -1,13 +1,36 @@
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
+import csv 
+import datetime
+import time
+import pytz
+
+
+def to_date(timestamp):
+    pst_tz = pytz.timezone('US/Pacific')
+    return datetime.datetime.fromtimestamp(timestamp, pst_tz)
+
+def get_hours(data):
+    return data.hour
 
 class Prediction:
     def __init__(self):
-    	self.alldata = {}
+        self.alldata = {}
 
     def preprocessing(self):
-    	path = 'first_data/data/'
+        path = 'first_data/data/'
+
         for filename in os.listdir(path):
-            print filename
+            # df = pd.DataFrame() 
+            df = pd.read_csv(path+filename)
+            print df.columns
+            # df.columns = ['time','a','b','c','d','label']
+            # print df
+            # print to_date(float(1521059843377)/1000.0)
+            df['time'].apply(lambda x: str(get_hours(to_date(float(x)/1000.0))))
+            print df
+            # with open(path+filename) as csvfile:
+            #   line = f.readline()
